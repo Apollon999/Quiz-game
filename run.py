54987d7ac6366c2fb0a6de69711f63a4700d973d
Apollon999 <diddec01@outlook.com>
2023-04-27 16:35:00 +0000

    Give the player a welcome message and an option to continue to start te game

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to my Sports Quiz!")
+
+confirm = input("Do you want to play? ")
+
+if confirm != "yes":
+    quit()
+
+print("Lets Play!")
