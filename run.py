c00faf75488bac80790f010c136312c3b878adab
Apollon999 <diddec01@outlook.com>
2023-04-27 19:53:08 +0000

    add error message for invalid start-confrimation

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ print("Welcome to my Sports Quiz!")
 
 confirm = input("Do you want to play? ")
 
-if confirm != "yes":
+if confirm.lower() != "yes":
+    print("Invalid input. Please type 'yes' to play.")
     quit()
 
 print("Lets Play!")
